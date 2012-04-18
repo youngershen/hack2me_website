81571e77d610d387436b1f0d5cdf391e9ea07334
Younger.Shen <younger.x.shen@gmail.com>
2012-04-18 10:23:55 +0800

    2ed commit

--- hack2me/settings.py
+++ hack2me/settings.py
@@ -119,7 +119,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hack2me',
-    'models',
+    'models'
     
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
